5716d0232db1ebd7472574c1e67a8e03b848a2ac
YooSangwon <ysw1230@gmail.com>
2022-03-09 21:57:11 +0900

    moveToRemote

---
+++ src/styled/component/flexItemC.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const FlexItemMainC = styled.main`
+  flex-basis: 30%;
+  background-color: antiquewhite;
+`;
+
+const FlexItemSectionC = styled.section`
+  flex-basis: 30%;
+  background-color: aquamarine;
+`;
+
+export { FlexItemMainC, FlexItemSectionC };
---
+++ src/styled/layout/flexContainerL.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import './reset.css';
+
+const FlexContainerL = styled.div`
+  height: 100vh;
+  width: 100vw;
+  overflow-y: auto;
+  display: flex;
+  flex-direction: column;
+  background-color: #fff;
+`;
+
+export { FlexContainerL };
