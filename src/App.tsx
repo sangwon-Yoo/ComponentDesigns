a83f5a73e13b8ede50cd37f5945372ad53038947
YooSangwon <ysw1230@gmail.com>
2022-03-12 19:53:40 +0900

    flex layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FlexContainerL } from "src/styled/layout/flexContainerL";
-import { FlexItemMainC, FlexItemSectionC } from "src/styled/component/flexItemC";
+import { FlexItemMainC, FlexItemSectionC } from "src/styled/layout/flexItemC";
 
 function App() {
   return (
--- src/styled/layout/flexContainerL.tsx
+++ src/styled/layout/flexContainerL.tsx
@@ -4,8 +4,8 @@ import './reset.css';
 const FlexContainerL = styled.div`
   height: 100vh;
   width: 100vw;
-  margin: auto;           //양쪽 여백이 생길 경우 auto를 통해 가운데 정렬
-  max-width: 1440px;
+  //margin: auto;           //양쪽 여백이 생길 경우 auto를 통해 가운데 정렬
+  //max-width: 1440px;
   overflow-y: auto;       //넘치면 스크롤
   display: flex;          
   flex-direction: column;
